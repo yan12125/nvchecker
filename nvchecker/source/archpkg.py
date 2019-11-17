163a53e7f9289fb453330504c6a92b88b8a49ed3
Chih-Hsuan Yen <yan12125@gmail.com>
2019-11-17 22:38:56 +0800

    archlinuxcn-testing

--- nvchecker/source/archpkg.py
+++ nvchecker/source/archpkg.py
@@ -11,12 +11,25 @@ URL = 'https://www.archlinux.org/packages/search/json/'
 
 get_cacheable_conf = conf_cacheable_with_name('archpkg')
 
+REPO_MAP = {
+  'stable': ['Core', 'Extra', 'Community', 'Multilib'],
+  'testing': ['Testing', 'Community-Testing', 'Multilib-Testing'],
+  'kde-unstable': ['KDE-Unstable'],
+}
+
 async def get_version(name, conf, **kwargs):
   pkg = conf.get('archpkg') or name
   strip_release = conf.getboolean('strip-release', False)
   provided = conf.get('provided')
 
-  async with session.get(URL, params={"name": pkg}) as res:
+  # TODO: undocumented in aiohttp - passing params as a sequence
+  params = [("name", pkg)]
+
+  repos = REPO_MAP[conf.get('channel', 'stable')]
+  for repo in repos:
+      params.append(("repo", repo))
+
+  async with session.get(URL, params=params) as res:
     data = await res.json()
 
   if not data['results']:
